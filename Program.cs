56734d7d3eb2fedd7e05a292c50777f8196856e4
sgaruda-sudo <st169945@stud.uni-stuttgart.de>
2024-01-21 20:19:23 +0100

    "First C# program"

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
